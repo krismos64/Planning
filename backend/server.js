22c37409e24138e14049f9de037704816a4ccc1f
krismos64 <c.mostefaoui@yahoo.fr>
2025-02-26 18:15:05 +0100

    modal

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,10 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5002",
+    origin: [
+      process.env.FRONTEND_URL || "http://localhost:5002",
+      "http://localhost:5004",
+    ],
     credentials: true,
   })
 );
